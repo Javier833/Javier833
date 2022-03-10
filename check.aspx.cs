ab67888d60bf1eb767699a3eb2c6f8dcbbed049d
Javier833 <81651159+Javier833@users.noreply.github.com>
2022-03-09 23:25:12 -0600

    Create check.aspx.cs

---
+++ check.aspx.cs
@@ -0,0 +1,130 @@
+       public void checkboxs(object sender, EventArgs e)
+        {
+            if (Check0.Checked==true)
+            {
+                Check1.Checked = true;
+            }
+
+            if (Check1.Checked == true)
+            {
+                Check0.Checked = true;
+            }
+
+
+
+            if (Check2.Checked == true)
+            {
+                Check0.Checked = true;
+                Check1.Checked = true;
+            }
+            if (Check3.Checked == true)
+            {
+                Check0.Checked = true;
+                Check1.Checked = true;
+                Check2.Checked = true;
+            }
+            if (Check4.Checked == true)
+            {
+                Check0.Checked = true;
+                Check1.Checked = true;
+                Check2.Checked = true;
+                Check3.Checked = true;
+            }
+            if (Check5.Checked == true)
+            {
+                Check0.Checked = true;
+                Check1.Checked = true;
+                Check2.Checked = true;
+                Check3.Checked = true;
+                Check4.Checked = true;
+            }
+            if (Check6.Checked == true)
+            {
+                Check0.Checked = true;
+                Check1.Checked = true;
+                Check2.Checked = true;
+                Check3.Checked = true;
+                Check4.Checked = true;
+                Check5.Checked = true;
+            }
+            if (Check7.Checked == true)
+            {
+                Check0.Checked = true;
+                Check1.Checked = true;
+                Check2.Checked = true;
+                Check3.Checked = true;
+                Check4.Checked = true;
+                Check5.Checked = true;
+                Check6.Checked = true;
+            }
+            if (Check8.Checked == true)
+            {
+                Check0.Checked = true;
+                Check1.Checked = true;
+                Check2.Checked = true;
+                Check3.Checked = true;
+                Check4.Checked = true;
+                Check5.Checked = true;
+                Check6.Checked = true;
+                Check7.Checked = true;
+            }
+            if (Check9.Checked == true)
+            {
+                Check0.Checked = true;
+                Check1.Checked = true;
+                Check2.Checked = true;
+                Check3.Checked = true;
+                Check4.Checked = true;
+                Check5.Checked = true;
+                Check6.Checked = true;
+                Check7.Checked = true;
+                Check8.Checked = true;
+            }
+            if (Check10.Checked == true)
+            {
+                Check0.Checked = true;
+                Check1.Checked = true;
+                Check2.Checked = true;
+                Check3.Checked = true;
+                Check4.Checked = true;
+                Check5.Checked = true;
+                Check6.Checked = true;
+                Check7.Checked = true;
+                Check8.Checked = true;
+                Check9.Checked = true;
+            }
+            if (Check11.Checked == true)
+            {
+                Check0.Checked = true;
+                Check1.Checked = true;
+                Check2.Checked = true;
+                Check3.Checked = true;
+                Check4.Checked = true;
+                Check5.Checked = true;
+                Check6.Checked = true;
+                Check7.Checked = true;
+                Check8.Checked = true;
+                Check9.Checked = true;
+                Check10.Checked = true;
+            }
+            if (Check12.Checked == true)
+            {
+                Check0.Checked = true;
+                Check1.Checked = true;
+                Check2.Checked = true;
+                Check3.Checked = true;
+                Check4.Checked = true;
+                Check5.Checked = true;
+                Check6.Checked = true;
+                Check7.Checked = true;
+                Check8.Checked = true;
+                Check9.Checked = true;
+                Check10.Checked = true;
+                Check11.Checked = true;
+            }
+
+
+
+
+
+        }
